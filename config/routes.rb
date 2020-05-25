fb9071cfad2cf2a42e0a64ea71054732880127c9
linhaishen <linhai.shen@outlook.com>
2020-05-25 14:56:59 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-	devise_for :users, path_names: { 
-		sign_up: '' #Stop Sign Up
-	}
+	#devise_for :users, path_names: { 		sign_up: '' #Stop Sign Up	}
+	devise_for :users
 
 	root to: "orders#index"
   
